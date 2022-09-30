7da83be3b1616485049dd8b2faa3b50ee1c651b8
Eduardo Nunez <edu.a.n.van.eyl@gmail.com>
2022-09-30 16:07:28 +0800

    Added a legend to the graph

--- code.js
+++ code.js
@@ -13,7 +13,8 @@ const svg = d3.select("#my_dataviz")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 //Read the data, originally at "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv"
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv").then( function(data) {
+//Try 2 working at "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv"
+d3.csv("http://vis.lab.djosix.com:2020/data/iris.csv").then( function(data) {
 
     // Declare X axis (but not add it to the graph yet!)
     const x = d3.scaleLinear()
@@ -27,7 +28,7 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/ir
         .range([ height, 0]);
     
     //Populate dropdown menus
-    var all_axis = ["Sepal_Width", "Sepal_Length", "Petal_Length", "Petal_Width"]
+    var all_axis = ["sepal width", "sepal length", "petal length", "petal width"]
     d3.select("#x_select_button")
         .selectAll('myOptions')
             .data(all_axis)
@@ -45,28 +46,34 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/ir
 
     // Color scale
     const color_strong_shade = d3.scaleOrdinal()
-        .domain(["setosa", "versicolor", "virginica"])
+        .domain(["Iris-setosa", "Iris-versicolor", "Iris-virginica"])
         .range(["#e6c10b", "#19c29a", "#bc19c2"])
     
     const color_light_shade = d3.scaleOrdinal()
-        .domain(["setosa", "versicolor", "virginica"])
+        .domain(["Iris-setosa", "Iris-versicolor", "Iris-virginica"])
         .range(["#eddc8a", "#82c2b3", "#c082c2"])
 
     //Function called for highlighting the selected (onhover) species
     var highlight = function(d){
         console.log("Log: user selected species: " + d3.select(this).attr("id"))
-
+        
+        try {
         d3.selectAll(".dot")
             .transition()
             .duration(50)
-            .style("fill", function (d) {return color_light_shade(d.Species)})
+            .style("fill", function (d) {return color_light_shade(d.class)})
             .attr("r", 3)
 
-        d3.selectAll("#"+d3.select(this).attr('id'))
+            d3.selectAll("#"+d3.select(this).attr('id'))
             .transition()
             .duration(50)
             .attr("r", 5)
-            .style("fill", function (d) {return color_strong_shade(d.Species)})
+            .style("fill", function (d) {return color_strong_shade(d.class)})
+        }
+        catch(err) {
+            console.log("Log: a point was highlighted and caused an error")
+        }
+        
 
     }
 
@@ -74,16 +81,16 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/ir
         d3.selectAll(".dot")
             .transition()
             .duration(50)
-            .style("fill", function (d) {return color_light_shade(d.Species)})
+            .style("fill", function (d) {return color_light_shade(d.class)})
             .attr("r", 3)
 
     }
 
     //Initialize default axis choice
-    var chosen_x_axis = "Sepal_Width"; //TODO change to match local csv file and not remote file
-    var chosen_y_axis = "Sepal_Width";  //TODO change to match local csv file and not remote file
+    var chosen_x_axis = "sepal width"; //TODO change to match local csv file and not remote file
+    var chosen_y_axis = "sepal width";  //TODO change to match local csv file and not remote file
 
-    // Add dots
+    // Add the graph
     function draw_graph() {
         //Add axis
         svg.append("g")
@@ -94,21 +101,45 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/ir
         svg.append("g")
             .call(d3.axisLeft(y));
 
+        //Draw dots
         svg.append('g')
             .selectAll("dot")
             .data(data)
             .enter()
             .append("circle")
                 .attr("class", function (d) {return "dot"}) //TODO: change to match local csv file and not remote file
-                .attr("id", function (d) {return d.Species})
+                .attr("id", function (d) {return d.class})
                 .attr("cx", function (d) { return x(d[chosen_x_axis]); } ) //TODO: change to match local csv file and not remote file
                 .attr("cy", function (d) { return y(d[chosen_y_axis]); } ) //TODO: change to match local csv file and not remote file
                 .attr("r", 4)
-                .style("fill", function (d) {return color_light_shade(d.Species)}) //TODO: change to match local csv file and not remote file
+                .style("fill", function (d) {return color_light_shade(d.class)}) //TODO: change to match local csv file and not remote file
             .on("mouseover", highlight)
             .on("mouseleave", remove_highlight)
 
+        //Add legend
+        var keys = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"]
+        svg.selectAll("mydots")
+            .data(keys)
+            .enter()
+            .append("circle")
+                .attr("cx", 290)
+                .attr("cy", function(d,i){return 38 + i*25})
+                .attr("r", 6)
+                .style("fill", function(d){return color_light_shade(d)})
         
+        svg.selectAll("mylabels")
+            .data(keys)
+            .enter()
+            .append("text")
+                .attr("x", 300)//function(d,i){return 30 + i*60})
+                .attr("y", function(d,i){return 40 + i*25})
+                .style("fill", function(d){return color_light_shade(d)})
+                .text(function(d) {return d})
+                .attr("text-anchor", "left")
+                .style("alignment-baseline", "middle")
+
+
+
         d3.selectAll('.dot').each(function() {
             console.log("Log: created instance of circle for species: " + d3.select(this).attr('id')); // Logs the id attribute.
         });
